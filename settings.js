74e00774c2d900e74d2557016ec1b35caf8ae164
ikhlaqak47 <164113448+ikhlaqak47@users.noreply.github.com>
2024-03-25 08:59:19 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@
 //                                                                                                      //
 //══════════════════════════════════════════════════════════════════════════════════════════════════════//
 //*
- //  * @project_name : XLICON-V3-MD
+ //  * @project_name : Ikhlaq' Bot-V3.0
  //  * @author : salmanytofficial
  //  * @youtube : https://www.youtube.com/@s4salmanyt
 //   * @description : XLICON-V3 ,A Multi-functional whatsapp user bot.
